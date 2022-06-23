e77404e1c50ec1bdee693055db8aa51feb724eac
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2022-06-23 10:44:28 +0200

    Create a Post Model and Migration

---
+++ database/migrations/2022_06_23_072123_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->integer('visits_count')->unsigned()->default(0);
+            $table->string('title')->nullable(false);
+            $table->string('slug')->nullable(false)->unique();
+            $table->text('excerpt');
+            $table->text('content');
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
