626f32f2448514a8b281fff7f4dbc18217337313
Jakub Pacanowski <jakub.pacanowski@gmail.com>
2022-06-23 11:24:19 +0200

    Create seed data to populate tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use \App\Models\User;
+use \App\Models\Post;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,5 +23,41 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::truncate();
+        Post::truncate();
+
+        $user = User::create([
+            'name' => 'falcon030',
+            'email' => 'falcon030@mail.com',
+            'password' => bcrypt('qwerty1234'),
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => 1,
+            'title' => 'Mastering Laravel',
+            'slug' => 'mastering-laravel',
+            'excerpt' => 'Lorem ipsum dolor sit amet.',
+            'content' => 'Lorem ipsum dolor sit amet.',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => 1,
+            'title' => 'Why I switched to OpenBSD',
+            'slug' => 'why-i-switched-to-openbsd',
+            'excerpt' => 'Lorem ipsum dolor sit amet.',
+            'content' => 'Lorem ipsum dolor sit amet.',
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => 1,
+            'title' => 'My history of learning ancient languages',
+            'slug' => 'my-history-of-learning-ancient-languages',
+            'excerpt' => 'Lorem ipsum dolor sit amet.',
+            'content' => 'Lorem ipsum dolor sit amet.',
+        ]);
     }
 }
